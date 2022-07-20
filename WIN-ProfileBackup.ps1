1db136d4b404fd3ab04d58ca4b010a0bae951724
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2022-07-20 00:57:58 -0500

    Update WIN-ProfileBackup.ps1

--- WIN-ProfileBackup.ps1
+++ WIN-ProfileBackup.ps1
@@ -6,8 +6,9 @@ Set-Location C:\Temp\UserBackup
 # Create an Exclude List
 $env:USERPROFILE
 ($env:USERPROFILE + "\appdata\temp") | Out-File -FilePath C:\excludelist.txt -Append
+($env:USERPROFILE + "\appdata\local\temp") | Out-File -FilePath C:\excludelist.txt -Append
 
-# Robocopy the files from the users profile to the temporary location
+# XCopy the files, all directories from the users profile to the temporary location
 $env:USERPROFILE
 $Destination = "C:\Temp\UserBackup"
 xcopy $env:USERPROFILE $Destination /s /c /h /EXCLUDE:c:\excludelist.txt
