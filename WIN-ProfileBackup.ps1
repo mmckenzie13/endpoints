f311734f51336d2fb4e57f48a23de77f744a1fc2
Matthew McKenzie <mmckenzie13@users.noreply.github.com>
2022-07-26 00:10:35 -0500

    Updated to exclude Outlook OST location

--- WIN-ProfileBackup.ps1
+++ WIN-ProfileBackup.ps1
@@ -7,6 +7,7 @@ Set-Location C:\Temp\UserBackup
 $env:USERPROFILE
 ($env:USERPROFILE + "\appdata\temp") | Out-File -FilePath C:\excludelist.txt -Append
 ($env:USERPROFILE + "\appdata\local\temp") | Out-File -FilePath C:\excludelist.txt -Append
+($env:USERPROFILE + "\appdata\local\Microsoft\Outlook") | Out-File -FilePath C:\excludelist.txt -Append
 
 # XCopy the files, all directories from the users profile to the temporary location
 $env:USERPROFILE
